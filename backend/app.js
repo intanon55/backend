6badfee32eeaee1d5661a2cb7d56450055382a8b
intanon55 <53069180+intanon55@users.noreply.github.com>
2020-11-09 10:59:53 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,6 @@ app.post('/api/v1/change-richmenu', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log("Ready on port 3000");
+app.listen(5000, () => {
+    console.log("Ready on port 5000");
 });
